fa24f6c80b7823c72ac186f57b74c5ffe76bb554
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-09-16 14:44:30 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,13 @@
-code will be here shortly
+function setup() {
+  createCanvas(300, 300);
+}
+
+function draw() {
+  background('#1B263B');
+  fill('#E0E1DD')
+  text("High Low",50,50)
+  push()
+    textSize(18)
+    text("make a guess",50,100)
+  pop()
+}

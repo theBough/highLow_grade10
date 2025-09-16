7fd4d4d706230ff2ed97cfc8e8ca24080e75cd1a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-09-16 15:02:17 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
-let myInput;
+let myInput,myButton;
 
 function setup() {
   createCanvas(300, 300);
   myInput = createInput();
   myInput.position(50,120)
+  
+  myButton = createButton("guess");
+  myButton.position(50,140)
 }
 
 function draw() {
   background('#1B263B');
   fill('#E0E1DD');
+  textSize(28)
   text("High Low",50,50);
   push();
     textSize(18);

be6495aea722612778378e689aaeab1412d9d81d
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-09-17 14:52:24 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,29 @@
-let myInput,myButton;
+let myInput, 
+    myButton,
+    answer = "nothing yet";
 
 function setup() {
   createCanvas(300, 300);
   myInput = createInput();
-  myInput.position(50,120)
-  
-  myButton = createButton("guess");
-  myButton.position(50,140)
-  myButton.mousePressed(getAnswer)
-}
+  myInput.position(50, 120);
 
+  myButton = createButton("guess");
+  myButton.position(50, 140);
+  myButton.mousePressed(getAnswer);
+  
+}
 function draw() {
-  background('#1B263B');
-  fill('#E0E1DD');
-  textSize(28)
-  text("High Low",50,50);
+  background("#1B263B");
+  fill("#E0E1DD");
+  textSize(28);
+  text("High Low", 50, 50);
   push();
-    textSize(18);
-    text("make a guess",50,100);
+  textSize(18);
+  text("make a guess", 50, 100);
+  text(answer,50,200);
   pop();
 }
-function getAnswer(){
-  
+function getAnswer() {
+  answer = myInput.value();
+  console.log(answer)
 }
